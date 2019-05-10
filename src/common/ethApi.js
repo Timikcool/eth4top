cffe52a885ff897b7f6fa15a11148341cafd8b26
Timikcool <timikcool@yandex.ru>
2019-05-10 22:40:51 +0300

    trying to deploy better

--- src/common/ethApi.js
+++ src/common/ethApi.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 import { ethers } from 'ethers';
 
-
 // export getTop() {
-    
+
 // }
 export async function connectToEth() {
-    window.ethereum.enable()
-    const address = await _getAddress();
-    console.log(address);
-
+  window.ethereum.enable();
+  const address = await _getAddress();
+  console.log(address);
 }
 
 function _getProvider() {
-    return window.web3 ? new ethers.providers.Web3Provider(window.web3.currentProvider) : ethers.getDefaultProvider('rinkeby');
+  return window.web3
+    ? new ethers.providers.Web3Provider(window.web3.currentProvider)
+    : ethers.getDefaultProvider('rinkeby');
 }
 
 function _getAddress(callback) {
-const provider = _getProvider();
-const signer = provider.getSigner();
-return signer.getAddress();
+  const provider = _getProvider();
+  const signer = provider.getSigner();
+  return signer.getAddress();
 }
