c888c67ef07ddf8f677f92b3128fe18bb966afeb
Pavel Belyakov <theflashofsonic@gmail.com>
2019-05-20 16:02:37 +0300

    smart contract development

--- smart-contract/contracts/EthTop.sol
+++ smart-contract/contracts/EthTop.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.25 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 // This is just a simple example of a coin-like contract.
 // It is not standards compatible and cannot be expected to talk to other
@@ -18,7 +19,7 @@ contract EthTop {
 		address author; // да
 	}
 
-  // массив для хранения постов
+  	// массив для хранения постов
 	Post[] public posts;
 
 	constructor() public {
@@ -28,8 +29,6 @@ contract EthTop {
 
 	// пост создан
 	event PostCreated(string _text, uint indexed _id, uint _price, address indexed _author);
-	// пост такой-то получил стлько-то денег
-	event PostUpdated(uint indexed _id, uint _price, address indexed _author);
 
 	function createPost(string memory _text) public payable returns (uint){
 		uint id = posts.push(
@@ -42,22 +41,70 @@ contract EthTop {
 			)
 		) - 1;
 		posts[id].id = id;
+		amount += msg.value;
 		emit PostCreated(_text, id, msg.value, msg.sender);
 		return id;
 	}
 
-	/*(function updatePost(uint _id) external payable {
-		uint money = msg.value;
-		amout += money;
-		posts[_id].price += money;// тут money уже может быть равен нулю
-	}*/
+	function sortByPrice() private view returns (Post[] memory) {
+		Post[] memory sortedPosts = posts;
+		Post memory tmpPost;
+		bool flag = true;
+		while(flag) {
+			flag = true;
+			for( uint i = 1; i < sortedPosts.length; i++ ) {
+				if( sortedPosts[i-1].price < sortedPosts[i].price ) {
+					tmpPost = sortedPosts[i-1];
+					sortedPosts[i-1] = sortedPosts[i];
+					sortedPosts[i] = tmpPost;
+					flag = false;
+				}
+			}
+		}
+		return sortedPosts;
+	}
 
-	/*function getPosts(uint _count ,uint _offset) external view {
-		return posts;
-	}*/
+	function getTopTenPosts() external view returns (Post[] memory) {
+		Post[] memory sortedPosts = sortByPrice();
+		uint size = sortedPosts.length < 10 ? sortedPosts.length : 10;
+		Post[] memory topTen = new Post[] (size);
 
-	/*function widthdraw() public {
-		require( msg.sender == owner);
-		owner.transfer( address(this).balance );
-	}*/
+		for( uint i = 0; i < size; i++) {
+			topTen[i] = sortedPosts[i];
+		}
+		return topTen;
+	}
+
+	function getTopTenIds() external view returns ( uint[] memory ) {
+		Post[] memory sortedPosts = sortByPrice();
+		uint size = sortedPosts.length < 10 ? sortedPosts.length : 10;
+		uint[] memory topTen = new uint[] (size);
+
+		for( uint i = 0; i < size; i++) {
+			if( i < sortedPosts.length ) {
+				topTen[i] = sortedPosts[i].id;
+			}
+		}
+		return topTen;
+	}
+
+	function getPosts(uint _count, uint _offset) external view returns( Post[] memory) {
+		Post[] memory result = new Post[] ( _count);
+		uint counter = 0;
+		for( uint i = _offset; i < _offset + _count ; i++ ) {
+			if( i < posts.length) {
+				result[counter] = posts[i];
+				counter++;
+			}
+		}
+	}
+
+	function getAmount() public view returns (uint) {
+		return amount;
+	}
+
+	function widthdraw() public {
+		require(msg.sender == owner, "Only owner can withdraw money from the contract");
+		address(uint160(address(this).balance)).transfer(amount);
+	}
 }
