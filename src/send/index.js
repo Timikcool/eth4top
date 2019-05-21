075e21af4583c77eb40f526c356ff12ad8f68bdc
Timikcool <timikcool@yandex.ru>
2019-05-21 15:35:32 +0300

    Added feed and payment components

--- src/send/index.js
+++ src/send/index.js
@@ -1,7 +1,16 @@
-const Pay = () => (
-    <span>pay</span>
-  );
-  
-  // usage
-  // render(<MyComponent name="Inferno" age={2}/>, container);
-  export default Pay
+import './send.scss';
+const Send = () => (
+  <div className="send-container">
+    <form class="send" onSubmit={console.log}>
+      <h4>Type your text here:</h4>
+      <input type="text" />
+      <h4>ETH to send:</h4>
+      <input type="text" />
+      <button type="submit">Proceed</button>
+    </form>
+  </div>
+);
+
+// usage
+// render(<MyComponent name="Inferno" age={2}/>, container);
+export default Send;
