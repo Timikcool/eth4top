99631700ad8b1a3c0e08d289f1a89b09354a69a0
Pavel Belyakov <theflashofsonic@gmail.com>
2019-05-22 15:44:13 +0300

    connection to web3 in the test react app

--- web3-tests/react-test/src/App.js
+++ web3-tests/react-test/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from './logo.svg';
 import './App.css';
+import ABI from './EthTop.json';
+const ADDRESS = '0x2cC088354fD84ebE937550897722dCf665f40eE5';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount = async () => {
+
+    // getting account via metamask
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+
+    // connection to the contract
+    const Top = new web3.eth.Contract(ABI.abi, ADDRESS);
+    console.log(Top);
+  }
+
+  render() {
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
