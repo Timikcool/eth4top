18f610a11b49cd22501d1c5115e7511b3faa912a
Timikcool <timikcool@yandex.ru>
2019-07-20 19:23:57 +0300

    Style and api improvements

--- front-end/src/common/ethApi.js
+++ front-end/src/common/ethApi.js
@@ -1,23 +1,87 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { ethers } from 'ethers';
+import Web3 from 'web3';
+import ABI from './abi.json';
+const ADDRESS = '0x05FAAE3F72c2Ea4FCd88B564644e071EbEa2de8a';
 
-// export getTop() {
-
-// }
-export async function connectToEth() {
+export const connectToEth = async () => {
+  //обязательный шаг
   window.ethereum.enable();
-  const address = await _getAddress();
-  console.log(address);
+  return await getAddress();
+};
+
+export const getContract = web3 => {
+  if (web3) {
+    return new web3.eth.Contract(ABI.abi, ADDRESS);
+  } else {
+    const web3 = getWeb3();
+    return new web3.eth.Contract(ABI.abi, ADDRESS);
+  }
+};
+
+export const getWeb3 = () => {
+  return new Web3(Web3.givenProvider || 'http://localhost:8545');
 }
 
-function _getProvider() {
+// получаем провайдер
+export const getProvider = () => {
   return window.web3
     ? new ethers.providers.Web3Provider(window.web3.currentProvider)
     : ethers.getDefaultProvider('rinkeby');
-}
+};
 
-function _getAddress(callback) {
-  const provider = _getProvider();
+// получаем адрес клиента
+export const getAddress = callback => {
+  const provider = getProvider();
   const signer = provider.getSigner();
   return signer.getAddress();
-}
+};
+
+// этим способом можно получить правильный порядок айдишников постов.
+export const getSortedPosts = async (web3, contract, callback) => {
+  const ids = await getContract()
+    .methods.getTopTenIds()
+    .call();
+
+  //тут по id получаем посты
+  await Promise.all(ids.map(id => this.getPost(contract, id))).then(result => {
+    const postsFormatted = result.map(post => this.normalizePost(post));
+    callback(postsFormatted);
+  });
+};
+
+// приводим пост к норм виду
+export const normalizePost = post => {
+  return {
+    text: post.text,
+    id: parseInt(post.id),
+    price: parseInt(post.price),
+    author: post.author,
+    time: parseInt(post.timestamp)
+  };
+};
+
+// получаем пост по id
+export const getPost = postId => {
+  return getContract()
+    .methods.posts(postId)
+    .call();
+};
+
+// создаём пост, вешаем обработчики
+export const createPost = (
+  account,
+  text,
+  ether,
+  confirmationCallback,
+  errorCallback
+) => {
+  getContract().methods
+    .createPost(text)
+    .send({
+      from: account,
+      value: window.web3.toWei(ether.toString(), 'ether')
+    })
+    .on('confirmation', confirmationCallback)
+    .on('error', errorCallback);
+};
--- front-end/src/send/index.js
+++ front-end/src/send/index.js
@@ -1,15 +1,69 @@
+import { Component } from 'inferno';
 import './send.scss';
-const Send = () => (
-  <div className="send-container">
-    <form class="send" onSubmit={console.log}>
-      <h4>Type your text here:</h4>
-      <input type="text" />
-      <h4>ETH to send:</h4>
-      <input type="text" />
-      <button type="submit">Proceed</button>
-    </form>
-  </div>
-);
+import { createPost, connectToEth, } from '../common/ethApi';
+class Send extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '', ethers: 0.1 };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    createPost(window.web3.eth.accounts[0], this.state.text, this.state.ethers, this.onConfirm.bind(this), this.onError.bind(this));
+
+  }
+
+  onConfirm(){
+    alert('success');
+  }
+
+  onError(err) {
+    console.log(err)
+  }
+
+  componentDidMount(){
+    connectToEth().then((address) => console.log(address));
+  }
+
+  render() {
+    return (
+      <div className="send-container">
+        <form class="send" onSubmit={this.handleSubmit}>
+          <h4>Type your text here:</h4>
+          <input
+            type="text"
+            value={this.state.text}
+            name="text"
+            onInput={this.handleChange}
+          />
+          <h4>How much of ETH you want to spend?</h4>
+          <input
+            type="number"
+            value={this.state.ethers}
+            name="ethers"
+            onInput={this.handleChange}
+            className="eth"
+            min="0"
+            step="any"
+          />
+          <button type="submit">Proceed</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 // usage
 // render(<MyComponent name="Inferno" age={2}/>, container);
