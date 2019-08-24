9b35c8c53f5d43d9a1c508008289a3cd2203e2fe
Timikcool <timikcool@yandex.ru>
2019-08-24 23:32:46 +0300

    Bugfixes

--- front-end/src/top/Post.js
+++ front-end/src/top/Post.js
@@ -6,7 +6,7 @@ export default ({ author, text, timestamp, price }) => (
     <div className="post-content">{text}</div>
     <div className="post-time-and-eth">
       <div className="time">{moment.unix(timestamp).format('YYYY MM DD hh:mm:ss')}</div>
-      <div className="eth">{window.web3.fromWei(price)}</div>
+      <div className="eth">{window.web3.fromWei(price.toString())}</div>
     </div>
   </div>
 );
